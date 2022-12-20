a1f2f59e00ba94dfaee36e535dc02fa23f9854a2
mattialicausi <licausivitomattia@gmail.com>
2022-12-20 13:57:14 +0100

    Aggiunto Migration per creare e aggiungere campi nella tabella comics nel database

---
+++ database/migrations/2022_12_20_124646_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('description');
+            $table->float('price');
+            $table->string('series', 60);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
